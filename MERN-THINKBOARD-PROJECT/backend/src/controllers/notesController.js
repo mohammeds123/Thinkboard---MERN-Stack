f078eb8eed87777665245640a2c14e0726f9a643
Mohammed <mohammedolx88@gmail.com>
2026-07-16 16:21:49 +0100

    Finished initial setup of the backend for the MERN-THINKBOARD-PROJECT. Added necessary routes and controllers for handling notes. The server is now set up to handle CRUD operations for notes. Also improved file structure and ensured that the backend is ready for further development and integration with the frontend.

---
+++ MERN-THINKBOARD-PROJECT/backend/src/controllers/notesController.js
@@ -0,0 +1,16 @@
+export function getAllNotes(req, res) {
+  res.status(200).send("you fetched some notes");
+
+}
+
+export function createNote(req, res) {
+  res.status(201).json({ message: "your post has been created successfully" });
+}
+
+export function updateNote(req, res) {
+  res.status(200).json({ message: "your note has been updated successfully" });
+}
+
+export function deleteNote(req, res) {
+  res.status(200).json({ message: "your note has been deleted successfully" });
+}
