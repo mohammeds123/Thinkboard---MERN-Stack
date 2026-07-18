2402693ac2690b4260e123b9d26793211d55a458
Mohammed <mohammedolx88@gmail.com>
2026-07-19 00:02:55 +0100

    Changes for previous commit

--- MERN-THINKBOARD-PROJECT/backend/src/server.js
+++ MERN-THINKBOARD-PROJECT/backend/src/server.js
@@ -1,11 +1,23 @@
 import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
+import connectDB from "./config/db.js";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+console.log(process.env.MONGO_URI);
 
 const app = express();
+const PORT = process.env.PORT || 5001;
+
+connectDB();
 
 app.use("/api/notes", notesRoutes);
 
-app.listen(5001, () => {
-  console.log("Server is running on port 5001");
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`);
 });
+
+// mohammedolx88_db_user
+// wsCf2I5WSzyz6Ssk
+// mongodb+srv://mohammedolx88_db_user:wsCf2I5WSzyz6Ssk@cluster0.pxsdscq.mongodb.net/?appName=Cluster0
