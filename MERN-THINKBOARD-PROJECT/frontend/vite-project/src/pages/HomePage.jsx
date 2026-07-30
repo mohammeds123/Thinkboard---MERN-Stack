d84f98921ecebdd7cfd57fb688c1b60b044c1a73
Mohammed <mohammedolx88@gmail.com>
2026-07-31 00:03:45 +0400

    Created Notecard file to route users to the contents of the note on a larger screen when clicked

--- MERN-THINKBOARD-PROJECT/frontend/vite-project/src/pages/HomePage.jsx
+++ MERN-THINKBOARD-PROJECT/frontend/vite-project/src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import RateLimitedUI from '../components/RateLimitedUI'
 import { useEffect } from 'react'
 import axios from 'axios'
 import { toast } from 'react-hot-toast'
+import NoteCard from '../components/NoteCard'
 
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false)
@@ -39,6 +40,17 @@ const HomePage = () => {
 
       {isRateLimited && <RateLimitedUI/>}
 
+      <div className="max-w-7xl mx-auto p-4 mt-6">
+        {loading && <div className="text-center text-primary py-10">Loading notes...</div>}
+
+        {notes.length > 0 && !isRateLimited && (
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+            {notes.map((note) => (
+              <NoteCard key={note._id}note = {note}/>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   )
 }
