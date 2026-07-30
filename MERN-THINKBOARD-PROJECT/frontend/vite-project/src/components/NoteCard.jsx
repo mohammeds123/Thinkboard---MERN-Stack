f5f6cb626e177e6396a6f96f429029f177cc625a
Mohammed <mohammedolx88@gmail.com>
2026-07-31 00:23:28 +0400

    Basic UI for notecard outline

--- MERN-THINKBOARD-PROJECT/frontend/vite-project/src/components/NoteCard.jsx
+++ MERN-THINKBOARD-PROJECT/frontend/vite-project/src/components/NoteCard.jsx
@@ -1,9 +1,10 @@
-import { PenSquareIcon } from 'lucide-react'
+import { PenSquareIcon, Trash2Icon } from 'lucide-react'
 import React from 'react'
+import {Link} from "react-router"
 
 const NoteCard = ({note}) => {
   return <Link to={'/note/${note._id}'}
-  className="card bg-base-100 hover:shadow-lg transition-all duration-200 border-t-4 border-solid border-[#00FF]">
+  className="card bg-base-100 hover:shadow-lg transition-all duration-200 border-t-4 border-solid border-[#D4AF37]">
     <div className ="card-body">
         <h3 className="card-title text-base-content">{note.title}</h3>
         <p className = "text-base-content/70 line-clamp-3">{note.content}</p>
@@ -11,8 +12,8 @@ const NoteCard = ({note}) => {
             <span className="text-sm text-base-content/60"> {note.createdAt} </span>
             <div className="flex items-center gap-1">
                 <PenSquareIcon className="size-4"/>
-                <button>
-                    
+                <button className="btn btn-ghost btn-xs text-error">
+                    <Trash2Icon className="size-4" />
                 </button>
             </div>
 
