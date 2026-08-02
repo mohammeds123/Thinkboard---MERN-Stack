4f804accb615dbeac2f7bd75c8108ab5af5afc0f
Mohammed <mohammedolx88@gmail.com>
2026-08-02 18:06:35 +0400

    Added a utility function to format dates in the NoteCard component. Updated the display of the note's creation date to use this new formatting function for better readability.

--- MERN-THINKBOARD-PROJECT/frontend/vite-project/src/components/NoteCard.jsx
+++ MERN-THINKBOARD-PROJECT/frontend/vite-project/src/components/NoteCard.jsx
@@ -1,6 +1,7 @@
 import { PenSquareIcon, Trash2Icon } from 'lucide-react'
 import React from 'react'
 import {Link} from "react-router"
+import { formatDate } from '../lib/utils.js'
 
 const NoteCard = ({note}) => {
   return <Link to={'/note/${note._id}'}
@@ -9,7 +10,7 @@ const NoteCard = ({note}) => {
         <h3 className="card-title text-base-content">{note.title}</h3>
         <p className = "text-base-content/70 line-clamp-3">{note.content}</p>
         <div className="card-actions justify-between items-center mt-4">
-            <span className="text-sm text-base-content/60"> {note.createdAt} </span>
+            <span className="text-sm text-base-content/60"> {formatDate(new Date)} </span>
             <div className="flex items-center gap-1">
                 <PenSquareIcon className="size-4"/>
                 <button className="btn btn-ghost btn-xs text-error">
