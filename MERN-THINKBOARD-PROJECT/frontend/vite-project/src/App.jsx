4a7ffec7e9e9ab4c3386c82ba16855adcd4ef889
Mohammed <mohammedolx88@gmail.com>
2026-08-03 23:12:37 +0400

    Added a radial gradient background to the main App

--- MERN-THINKBOARD-PROJECT/frontend/vite-project/src/App.jsx
+++ MERN-THINKBOARD-PROJECT/frontend/vite-project/src/App.jsx
@@ -6,12 +6,14 @@ import NoteDetailPage from "./pages/NoteDetailPage.jsx"
 import { toast } from 'react-hot-toast'
 const App = () => {
   return (
-    <div data-theme="luxury">
-        <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/create" element={<CreatePage />} />
-            <Route path="/note/:id" element={<NoteDetailPage />} />
-        </Routes>
+    <div className="relative h-full w-full">
+      <div className="absolute inset-0 -z-10 h-full w-full
+[background:radial-gradient(125%_125%_at_50%_10%,#0F172A_45%,#7C3AED30_100%)]" />
+      <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/create" element={<CreatePage />} />
+          <Route path="/note/:id" element={<NoteDetailPage />} />
+      </Routes>
     </div>
   )
 }
--- MERN-THINKBOARD-PROJECT/frontend/vite-project/tailwind.config.js
+++ MERN-THINKBOARD-PROJECT/frontend/vite-project/tailwind.config.js
@@ -11,6 +11,6 @@ export default {
   },
   plugins: [daisyui],
   daisyui: {
-    themes: ["luxury", "forest", "business"],
+    themes: ["luxury"],
   }
 }
