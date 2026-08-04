5fe38a193166649feabbeca8a5074463c5b091da
Mohammed <mohammedolx88@gmail.com>
2026-08-04 19:32:46 +0400

    Created basic structure for the CreatePage component, including a back button and state management for title and content inputs.

--- MERN-THINKBOARD-PROJECT/frontend/vite-project/src/pages/CreatePage.jsx
+++ MERN-THINKBOARD-PROJECT/frontend/vite-project/src/pages/CreatePage.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import {Link} from "react-router"
+import {ArrowLeftIcon} from "lucide-react"
+import {useState} from 'react'
 
 const CreatePage = () => {
+  const [title, setTitle] = useState("")
+  const [content, setContent] = useState("")
+  const [loading, setLoading] = useState(false)
+
+  const handleSubmit = () => {}
   return (
-    <div>CreatePage</div>
+    <div className="min-h-screen beg-base-200">
+      <div className="container mx-auto px-4 py-8">
+        <div className="max-w-2xl mx-auto">
+          <Link to={"/"} className="btn btn-ghost mb-6">
+          <ArrowLeftIcon className="size-5"/>
+          </Link>
+        </div>
+      </div>
+    </div>
   )
 }
 
