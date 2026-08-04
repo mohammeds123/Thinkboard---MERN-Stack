1e1f3355e5bd971253cafa814de0e03280c3f809
Mohammed <mohammedolx88@gmail.com>
2026-08-05 01:07:25 +0400

    Created functionality for creating a new note and accounted for errors that may occur using try catch block

--- MERN-THINKBOARD-PROJECT/frontend/vite-project/src/pages/CreatePage.jsx
+++ MERN-THINKBOARD-PROJECT/frontend/vite-project/src/pages/CreatePage.jsx
@@ -3,18 +3,34 @@ import {Link} from "react-router"
 import {ArrowLeftIcon} from "lucide-react"
 import {useState} from 'react'
 import { toast } from 'react-hot-toast'
+import { useNavigate } from 'react-router'
+import axios from 'axios'
+
 
 const CreatePage = () => {
   const [title, setTitle] = useState("")
   const [content, setContent] = useState("")
   const [loading, setLoading] = useState(false)
-
-  const handleSubmit = (e) => {
+  const navigate = useNavigate()
+  const handleSubmit = async (e) => {
     e.preventDefault()
     
     if(!title.trim() || !content.trim()){
       toast.error("All fields are required")
       return
+    }
+
+    setLoading(true)
+    try{
+      await axios.post("http://localhost:5001/api/notes", {title, content})
+      toast.success("Note created successfully!")
+      navigate("/")
+    } catch (error) {
+      toast.error("Error creating note")
+      console.log("Error creating note:", error)
+    }
+    finally{
+      setLoading(false)
     }
   }
 
@@ -30,33 +46,33 @@ const CreatePage = () => {
             <div className="card-body">
               <h2 className="card-title text-2xl mb-4">Create New Note</h2>
               <form onSubmit={handleSubmit}>
-                <div ClassName="form-control mb-4">
+                <div className="form-control mb-4">
                   <label className="label">
                     <span className="label-text">Title</span>
                   </label>
                   <input 
                   type ="text"
                   placeholder="Note Title"
-                  className="input input-bordered w-full"
+                  className="input input-bordered"
                   value={title}
                   onChange={(e) => setTitle(e.target.value)}
                   />
                 </div>
 
-                <div ClassName="form-control mb-4">
+                <div className="form-control mb-4">
                   <label className="label">
                     <span className="label-text">Content</span>
                   </label>
                   <input type ="text"
                   placeholder="Write your Note here..."
-                  className="textarea textarea-bordered h-32 w-full"
+                  className="textarea textarea-bordered h-32"
                   value={content}
                   onChange={(e) => setContent(e.target.value)}
                   />
                 </div>
 
                 <div className="card-actions justify-end">
-                  <button type="submit" className="btn btn-primary mt-5" disabled={loading}>
+                  <button type="submit" className="btn btn-primary" disabled={loading}>
                     {loading ? "Creating..." : "Create Note"}
                   </button>
                 </div>
