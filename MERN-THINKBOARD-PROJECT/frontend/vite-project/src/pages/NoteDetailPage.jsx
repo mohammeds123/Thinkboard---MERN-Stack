b37fc7e801766dcc2c1eb9cf7981c059cc205c01
Mohammed <mohammedolx88@gmail.com>
2026-08-05 14:59:18 +0400

    Created basic fetching functionality involved in note detail page section

--- MERN-THINKBOARD-PROJECT/frontend/vite-project/src/pages/NoteDetailPage.jsx
+++ MERN-THINKBOARD-PROJECT/frontend/vite-project/src/pages/NoteDetailPage.jsx
@@ -1,8 +1,48 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import { useParams, useNavigate } from 'react-router'
+import axiosInstance from '../lib/axios'
+import { toast } from 'react-hot-toast'
+import { LoaderIcon } from 'lucide-react'
 
 const NoteDetailPage = () => {
+  const [note, setNote] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [saving, setSaving] = useState(false)
+
+  const navigate = useNavigate()
+  const { id } = useParams()
+
+  useEffect(() => {
+    const fetchNote = async () => {
+      try{
+        const res= await axiosInstance.get(`/notes/${id}`)
+        setNote(res.data)
+      } catch(error){
+        toast.error('Failed to load note')
+        console.error('Error fetching note:', error)
+      }
+      finally{
+        setLoading(false)
+      }
+    }
+    fetchNote()
+  }, [id])
+
+  if (loading) {
+    return (
+      <div className="min-h-screen bg-base-200 flex items-center justify-center">
+        <LoaderIcon className="animate-spin size-10" />
+      </div>
+    )
+  }    
+
   return (
-    <div>NoteDetailPage</div>
+    <div className="min-h-screen bg-base-200">
+      <div className="container mx-auto px-4 py-8">
+        
+      </div>
+    </div>
   )
 }
 
