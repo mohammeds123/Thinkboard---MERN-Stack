15297b29811679ae6b06f8da560fab0c14160548
Mohammed <mohammedolx88@gmail.com>
2026-08-05 14:40:50 +0400

    Added initial UI screen for when there are no notes including a button that takes you to creating your first note

--- MERN-THINKBOARD-PROJECT/frontend/vite-project/src/pages/HomePage.jsx
+++ MERN-THINKBOARD-PROJECT/frontend/vite-project/src/pages/HomePage.jsx
@@ -5,6 +5,7 @@ import { useEffect } from 'react'
 import axiosInstance from '../lib/axios'
 import { toast } from 'react-hot-toast'
 import NoteCard from '../components/NoteCard'
+import NotesNotFound from '../components/NotesNotFound'
 
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false)
@@ -42,6 +43,8 @@ const HomePage = () => {
 
       <div className="max-w-7xl mx-auto p-4 mt-6">
         {loading && <div className="text-center text-primary py-10">Loading notes...</div>}
+
+        {notes.length === 0 && !isRateLimited && <NotesNotFound/>}
 
         {notes.length > 0 && !isRateLimited && (
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
